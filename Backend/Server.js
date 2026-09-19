56bfcf7cd7b7c0695980e9e34bf52f38c69ffb41
sunnyshah011 <sunnyshah35350@gmail.com>
2026-09-19 13:53:24 +0545

    express setup

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import multer from "multer";
+import cors from "cors"
+import "dotenv/config"
+import connectDB from "./config/db.js";
 
 const App = express();
 const port = 8000;
 
+//middleware
+App.use(cors())
+App.use(express.json())
+App.use(multer().none())
+
 App.get("/", (req, res) => {
   res.send("server is running");
 });
+
+await connectDB()
 
 App.listen(port, () => {
   console.log("server running on port :: ", port);
